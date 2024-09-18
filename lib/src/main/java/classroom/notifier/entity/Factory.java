1dbc5ba406364ed08693b98489b9a29cbf1f81e6
Gabriel <gabrielmcajal@gmail.com>
2024-09-18 08:29:16 -0300

    Version 0

---
+++ lib/src/main/java/classroom/notifier/entity/Factory.java
@@ -0,0 +1,16 @@
+package classroom.notifier.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Factory {
+
+	public Set<MedioNotificacion> BuscarNotificadores(){
+		Set<MedioNotificacion> notificadores = new HashSet<MedioNotificacion>();
+		
+		
+		return notificadores;
+	}
+	
+	
+}
