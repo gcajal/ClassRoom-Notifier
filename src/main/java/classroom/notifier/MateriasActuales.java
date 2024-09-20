754915d1416ba3881d6b5fed75e7a141b7e189ec
Gabriel <gabrielmcajal@gmail.com>
2024-09-20 15:31:40 -0300

    Recibir información desde origen
    Cargar variables con la lectura de los archivos al iniciar la clase de Materias Actuales

--- src/main/java/classroom/notifier/MateriasActuales.java
+++ src/main/java/classroom/notifier/MateriasActuales.java
@@ -15,16 +15,20 @@ public class MateriasActuales extends Observable implements NotifierListeners {
     private Map<String, String> Asignacion;
     private Set<String> AsignacionNotificar;
     private Factory<MedioNotificacion> Factory;
+    //private LecturaDatos Database;
 
     MateriasActuales(LecturaDatos Database) {
         this.Asignacion = new HashMap<>();
         this.AsignacionNotificar = new HashSet<>();
         this.Factory = new Factory<>(System.getProperty("user.dir"));
+        //this.Database = Database;
+        Asignacion = Database.ListarMateriasAulas();
+        AsignacionNotificar = Database.ListarMateriasInscriptas()
     }
 
     protected void ComprobarNovedades(Map<String, String> AsignacionNueva) {
-        Asignacion.put("001", "7000");
-        AsignacionNotificar.add("001");
+        //Asignacion.put("001", "7000");
+        //AsignacionNotificar.add("001");
 
         Map<String, String> diferencia = AsignacionNueva.entrySet().stream().filter(nuevo -> this.AsignacionNotificar.contains(nuevo.getKey()))
                 .filter(nuevo -> this.Asignacion.getOrDefault(nuevo.getKey(), "") != nuevo.getValue())
