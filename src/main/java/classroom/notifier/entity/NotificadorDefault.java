1ed9afd6a26656563f4b7263ee1724418a9e11a5
Nicolas9300 <64173877+Nicolas9300@users.noreply.github.com>
2024-09-19 21:45:40 -0300

    Update NotificadorDefault.java

--- src/main/java/classroom/notifier/entity/NotificadorDefault.java
+++ src/main/java/classroom/notifier/entity/NotificadorDefault.java
@@ -2,13 +2,14 @@ package classroom.notifier.entity;
 
 import java.util.Map;
 
-class NotificadorDefault implements MedioNotificacion {
+class NotificadorDefault extends Observable implements MedioNotificacion {
 	
 	public NotificadorDefault() {
 		
 	}
 	//Se puede agregar datos del destinatario
 	public void Notificar(Map<String, String> cambiosANotificar) {
-		
+		setChanged();
+		notifyObservers(cambiosANotificar);
 	}
 }
